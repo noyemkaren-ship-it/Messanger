b9c88b1212d73be6918aeefc9b6991fed911af06
hacker <hacker@DESKTOP-TT1LKGV.Dlink>
2026-05-26 02:58:50 +0300

    new update

--- mess/src/main/java/com/example/mess/repo/ChatRepo.java
+++ mess/src/main/java/com/example/mess/repo/ChatRepo.java
@@ -1,5 +1,14 @@
 package com.example.mess.repo;
 
-public class ChatRepo {
-     
+import com.example.mess.models.Chat;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ChatRepo extends CrudRepository<Chat, Long> {
+     List<Chat> findByFirstUser(String firstUser);
+
+     Chat findByFirstUserAndSecondUser(String firstUser, String secondUser);
+
 }
--- mess/src/main/java/com/example/mess/repo/UserRepo.java
+++ mess/src/main/java/com/example/mess/repo/UserRepo.java
@@ -1,5 +1,10 @@
 package com.example.mess.repo;
 
+import com.example.mess.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class UserRepo {
-     
+
 }
