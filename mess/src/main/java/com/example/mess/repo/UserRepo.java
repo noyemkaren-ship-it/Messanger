fd105eab6f14a8e3fa1bb0ba10211ecc7a0e5af8
hacker <hacker@DESKTOP-TT1LKGV.Dlink>
2026-05-26 02:59:42 +0300

    new update

--- mess/src/main/java/com/example/mess/repo/UserRepo.java
+++ mess/src/main/java/com/example/mess/repo/UserRepo.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepo {
+public interface UserRepo extends CrudRepository<User, Long> {
 
 }
