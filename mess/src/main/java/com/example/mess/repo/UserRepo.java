5ddde7e511f7f060f9ec0fa84bb1552665fd775b
hacker <hacker@DESKTOP-TT1LKGV.Dlink>
2026-05-26 03:00:49 +0300

    new update

--- mess/src/main/java/com/example/mess/repo/UserRepo.java
+++ mess/src/main/java/com/example/mess/repo/UserRepo.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepo extends CrudRepository<User, Long> {
-
+     User findByNameAndPassword(String name, String password);
 }
