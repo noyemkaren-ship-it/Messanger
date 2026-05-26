297defe07430218ddfac735bf7e3c52961714c44
hacker <hacker@DESKTOP-TT1LKGV.Dlink>
2026-05-26 13:55:05 +0300

    new update

---
+++ mess/src/main/java/com/example/mess/serveces/ChatService.java
@@ -0,0 +1,27 @@
+package com.example.mess.serveces;
+
+import org.springframework.stereotype.Service;
+import com.example.mess.repo.ChatRepo;
+import com.example.mess.models.Chat;
+
+@Service
+public class ChatService {
+
+    private final ChatRepo chatRepo;
+
+    public ChatService(ChatRepo chatRepo) {
+        this.chatRepo = chatRepo;
+    }
+
+    public Chat getChatByFirstAndSecondUser(String firstUser, String secondUser) {
+        return chatRepo.findByFirstUserAndSecondUser(firstUser, secondUser);
+    }
+
+    public Chat saveChat(Chat chat) {
+        return chatRepo.save(chat);
+    }
+
+    public Iterable<Chat> getAllChats() {
+        return chatRepo.findAll();
+    }
+}
