0d4dd980d324237a254db66e4137b6fc7d6f9483
hacker <hacker@DESKTOP-TT1LKGV.Dlink>
2026-05-26 13:15:14 +0300

    new update

--- mess/src/main/java/com/example/mess/repo/MessageRepo.java
+++ mess/src/main/java/com/example/mess/repo/MessageRepo.java
@@ -1,5 +1,13 @@
 package com.example.mess.repo;
 
-public class MessageRepo {
-     
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import com.example.mess.models.Messang;
+import java.util.List;
+
+@Repository
+public interface MessageRepo extends CrudRepository<Messang, Long> {
+     Messang findByChatname(String chatname);
+
+     List<Messang> findByChatAndFrom(String chatname, String from);
 }
