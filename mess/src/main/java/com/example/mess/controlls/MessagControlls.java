f7b4172073dec4f20eabded664dbc37b1304eb4b
hacker <hacker@DESKTOP-TT1LKGV.Dlink>
2026-05-26 14:09:59 +0300

    new update

---
+++ mess/src/main/java/com/example/mess/controlls/MessagControlls.java
@@ -0,0 +1,5 @@
+package com.example.mess.controlls;
+
+public class MessagControlls {
+
+}
---
+++ mess/src/main/java/com/example/mess/controlls/UserControlls.java
@@ -0,0 +1,34 @@
+package com.example.mess.controlls;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.mess.serveces.UserServeces;
+import com.example.mess.models.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class UserControlls {
+
+     private final UserServeces userServeces;
+
+     UserControlls(UserServeces userServeces) {
+          this.userServeces = userServeces;
+     }
+
+     @PostMapping("/create/user")
+     public User createUser(@RequestBody User user) {
+          if (userServeces.findByName(user.getName()) == null) {
+               return userServeces.saveUser(user);
+          } else {
+               return null;
+          }
+     }
+
+     @GetMapping("/login")
+     public User login(@RequestParam String name, @RequestParam String password) {
+          return userServeces.findByNameAndPassword(name, password);
+     }
+
+}
